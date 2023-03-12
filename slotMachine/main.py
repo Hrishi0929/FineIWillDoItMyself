1f8b5b2082a026e90078dfe2205d8a6230995c8e
hrishiw <32129358+hrishiw@users.noreply.github.com>
2023-03-12 19:07:18 +0530

    added some more code to the slot machine

--- slotMachine/main.py
+++ slotMachine/main.py
@@ -1,3 +1,5 @@
+########## read the comments that have a single# in front of them first then go through the program then read the comments with the ##
+
 #  so we're actually going to build a text based slot machine.
 # Now, the way this will work is the user will deposit a certain amount of money. We're then going to allow them to bet on either one,
 # two or three lines of the slot machine, just to keep it pretty simple. I know in real slot machines they have a lot more lines than that
@@ -15,9 +17,58 @@
 # and for this we neeed to get the deposit the user is entering and the bet
 # those are the 2 things that we need before we start using the slot machine
 
+import random  # because we need to generate the slot machine values kind of randomly, right?
+
+
+## Well, the first thing we need to figure out is how many items we want to have in each reel and how long we want the lines to be.
+## Now, slot machines can get a bit complicated in terms of how the lines work. I'm going to keep this really simple,
+## and we're going to imagine that we have a three by three slot machine and that you only get a line if you get three in a row.
+## Okay, we had three in a row, then you win. This might not be the most balanced slot machine. It might not be one you want to play on, but for this project is fine.
+
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+ROWS = 3
+COLS = 3
+
+## All right. Now, what we need to specify is how many symbols are in each of our reels.
+## Now, it should be the same, at least from what I know. It should be the same number of symbols in every single reel.
+## We're not doing anything really complicated. When I say real, I'm talking about kind of one column, right? So how many symbols are in that column?
+## Because we're going to have to randomly select out of those symbols, and then we need values for our different symbols.
+## So we need to pick kind of first of all, how many symbols do we want to have in total and what do we want those symbols to be now
+## to keep this easy, we can do something just like, you know, ABCD. I think those are probably fine as the symbols.
+
+symbolCount = {"A": 2, "B": 4, "C": 6, "D": 8}
+
+## now we need something that will generate what the outcome of the slot machine is using the values of symbolCount and ROWS and COLS
+## what we need to do is generate what symbols are going to be in each column
+## based on the frequency of symbols that we have here. So we essentially need to randomly pick the number
+## of rows inside of each column. So if we have three rows, need to pick three symbols
+## that go inside of each of the columns that we have. And for each column we're doing kind of a new random pick, right? Or new random generation of the symbols.
+def getSlotMachineSpin(rows, cols, symbols):
+    # Now, the easiest way to randomly select
+    # values here for each of our columns is going to be to create a list that contains all of the different values we possibly could select from.
+    # And then to randomly choose three of those values and when we choose a value, will remove it from the list, and then we'll choose again.
+    allSymbols = []
+
+    # So what's going to happen here is I'm going to loop through this dictionary. Let's imagine I'm on the first key value pair.
+    #  My symbol is going to be "A" and my symbol count is going to be two.
+    # All right. So then I have another for loop inside of here where I'm looping through the symbol count.
+    # So the symbol count is two, and what I'm doing is doing this two times. So I'm going to add this symbol twice into my all symbols list.
+    for (
+        symbol,
+        symbolCount,
+    ) in (
+        symbol.items()
+    ):  # .items will give us both the key and value associated with a dictionary
+        for _ in range(symbolCount):
+            allSymbols.append(symbol)
+
+    # not that we have the allSymbols list now we have to decide what values go inside each of COLS
+
+    columns = [[], [], []]
+
 
 # this function is gonna be responsible for collecting user input
 # that gets the deposit from user
@@ -78,8 +129,16 @@ def getBetOnEachLine():
 def main():
     balance = deposit()
     lines = getNumberOfLines()
-    getBet = getBetOnEachLine()
-    totalBet = getBet * lines
+    # we need to check if the amount that they are betting is less than or equal to their balance
+    while True:
+        getBet = getBetOnEachLine()
+        totalBet = getBet * lines
+        if totalBet > balance:
+            print(
+                f"you donot have enough to bet that amount, Your currenct balance is ${balance}."
+            )
+        else:
+            break
     print(f"You are betting ${getBet} on {lines}. Total bet is equal to: ${totalBet} ")
 
 
